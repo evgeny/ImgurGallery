e736c5ac8d99d5e89d59c34b560243543ac89741
Evgeny Zinovyev <e.zinovyev@gmail.com>
2018-09-16 13:42:13 +0200

    fetch data through via rx stream and provide to ui via livedata, add glide generated api

--- app/src/main/java/com/ezino/imgurgallery/viewmodles/ImageListViewModel.kt
+++ app/src/main/java/com/ezino/imgurgallery/viewmodles/ImageListViewModel.kt
@@ -1,18 +1,50 @@
 package com.ezino.imgurgallery.viewmodles
 
 import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.LiveDataReactiveStreams
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.util.Log
 import com.ezino.imgurgallery.GalleryInteractor
 import com.ezino.imgurgallery.model.Image
 import com.ezino.imgurgallery.network.ImgurRepositoryImpl
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
-class ImageListViewModel: ViewModel() {
+class ImageListViewModel : ViewModel() {
     // todo replace by di
     private val galleryInteractor = GalleryInteractor(ImgurRepositoryImpl())
 
-    fun getImages(): LiveData<List<Image>> {
+    private val disposables = CompositeDisposable()
+
+    private val imageList = MutableLiveData<List<Image>>()
+
+
+    /*fun getImages(): LiveData<List<Image>> {
         return LiveDataReactiveStreams
                 .fromPublisher(galleryInteractor.imageList("hot", true))
+    }*/
+
+    fun getImages(): LiveData<List<Image>> {
+        if (imageList.value == null) {
+            loadImages()
+        }
+
+        return imageList
+    }
+
+    private fun loadImages() {
+        val disposable = galleryInteractor.imageStream("hot", true)
+                .toList().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { list -> imageList.setValue(list) },
+                        { error -> Log.e("safdsf", "todo show error", error) }
+                )
+
+        disposables.add(disposable)
+    }
+
+    override fun onCleared() {
+        disposables.clear()
     }
 }
